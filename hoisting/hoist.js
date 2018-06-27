37b592a0365ed30a00a10143139f97af06118426
Syed Mohammed Younus Mulla Maqbul Basha <younusm@softcrylic.co.in>
2018-06-27 16:20:22 +0530

    Hoisting in js

---
+++ hoisting/hoist.js
@@ -0,0 +1,24 @@
+function displayName() {
+    // console.log('Name' + name)
+    name = 'Syed';
+
+}
+displayName();
+
+// var myName;
+var myName = ''
+function myName() {
+    console.log('Syed');
+}
+console.log(typeof (myName))
+
+//Not Hoisted
+var myName = function () {
+    console.log("Rich");
+}
+
+
+
+var input = 2435
+
+console.log(input % 10)
